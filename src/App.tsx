1c8322fa081d37151384d9f70558bbd3185a5771
Henry <henrywl@gmail.com>
2024-09-04 20:22:12 +0100

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useContext, useEffect } from "react";
 import Login from "./pages/Login";
 import authContext from "./context/auth-context";
 import { Route, Routes, useNavigate } from "react-router-dom";
+import Example from "./components/Navbar";
+import Events from "./pages/Events";
 
 const App = () => {
   const auth = useContext(authContext)
@@ -19,13 +21,14 @@ const App = () => {
 
       {/* {!auth.isLoggedIn && <Login />}
       {auth.isLoggedIn && <h1>Events</h1>} */}
-
+      <Example/>
       <Routes>
       <Route path="/" element={<div>
         <h1>Logged in</h1>
         <button onClick={() => auth.logout()}>logout</button>
       </div>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/events" element={<Events/>}/> 
       </Routes>
     </div>
   );
