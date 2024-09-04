150bacbe6ef71ae5d6f3e57638071bd719521673
Henry <henrywl@gmail.com>
2024-09-04 20:34:27 +0100

    very basic get all events

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 type Props = {}
 
 function Events({}: Props) {
+const [events, setEvents] = useState([])
+
+useEffect(() => {
+    const getEvents = async () => {
+        const response = await axios.get(`http://localhost:3000/events/`)
+        console.log(response)
+
+        setEvents(response.data.allEvents)
+    }
+
+    getEvents()
+
+
+
+}, [])
+
   return (
-    <div>Events</div>
+    <div>
+        <h1>Events</h1>
+
+        
+        <div>
+            {events.map((event) => {
+                return <h1 key={event._id}>{event.name}</h1>
+            })}
+        </div>
+
+    </div>
   )
 }
 
