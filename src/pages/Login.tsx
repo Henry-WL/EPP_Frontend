bf50316ca6ed65f6414d9bd869254c42121b2d4b
Henry <henrywl@gmail.com>
2024-09-04 16:02:45 +0100

    guest login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -35,6 +35,13 @@ const Login = (props: Props) => {
     }
   }
 
+  const guestLogin = () => {
+    setEmail("henry@x.com")
+    setPassword("123456789")
+
+    loginSignupHandler()
+  }
+
 
 
   return (
@@ -111,7 +118,7 @@ const Login = (props: Props) => {
                     id="password"
                     name="password"
                     type="password"
-                    required
+                    // required
                     onChange={(e) => setPassword(e.target.value)}
                     autoComplete="current-password"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -128,6 +135,14 @@ const Login = (props: Props) => {
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Sign in
+                </button>
+
+                <button
+                  // type="submit"
+                  className="mt-4 flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={guestLogin}
+                >
+                  Guest Sign in
                 </button>
               </div>
             </form>
