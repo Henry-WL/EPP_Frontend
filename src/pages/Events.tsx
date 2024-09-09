4a7c3c21b15e298cc50fb6f5cc66044fb79084d7
Henry <henrywl@gmail.com>
2024-09-09 14:45:32 +0100

    basic httphook

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,36 +1,62 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import EventCard from '../components/EventCard'
+import { useHttpClient } from '../components/hooks/http-hook'
 
 type Props = {}
 
 function Events({}: Props) {
 const [events, setEvents] = useState([])
+const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+
+console.log('hello')
 
 useEffect(() => {
-    const getEvents = async () => {
-        const response = await axios.get(`http://localhost:3000/events/`)
-        console.log(response)
+    // const getEvents = async () => {
+    //     const response = await axios.get(`http://localhost:3000/events/`)
+    //     console.log(response)
 
-        setEvents(response.data.allEvents)
+    //     setEvents(response.data.allEvents)
+    // }
+
+    // getEvents()
+
+
+    const fetchEvents = async () => {
+        try {
+            const responseData = await sendRequest('http://localhost:3000/events/')
+            console.log(responseData)
+            setEvents(responseData.allEvents)
+        }
+
+        catch (err) {
+            console.log(err)
+        }
+
     }
+    
+    fetchEvents()
 
-    getEvents()
 
-
-
-}, [])
+}, [sendRequest])
 
   return (
     <div>
         <h1>Events</h1>
 
+        {isLoading && (
+            <div><h1 className='text-3xl'>Loading....</h1></div>
+        )}
+
+        {!isLoading &&
         
         <div className='flex gap-4'>
             {events.map((event) => {
                 return <EventCard eventName={event.name} eventLocation={event.location} id={event._id}/>
             })}
         </div>
+        }
 
     </div>
   )
