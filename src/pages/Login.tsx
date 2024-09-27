6de5025c1999cd02ad5b7477f8aaa8601a3177fc
Henry <henrywl@gmail.com>
2024-09-27 01:51:21 +0100

    styling for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -105,7 +105,7 @@ const Login = (props: Props) => {
                     // required
                     onChange={(e) => setEmail(e.target.value)}
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
@@ -135,7 +135,7 @@ const Login = (props: Props) => {
                     // required
                     onChange={(e) => setPassword(e.target.value)}
                     autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
 
                   {error && <p className="text-red-500">{error}</p>}
