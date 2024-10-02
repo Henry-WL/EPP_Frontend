c1f60d3630ab5720917900800ddc5576033e782a
Henry <henrywl@gmail.com>
2024-10-02 02:25:29 +0100

    style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -85,11 +85,12 @@ const Login = () => {
       <div className="w-1/2">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
+            {/* <img
+              alt="EPP"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               className="mx-auto h-10 w-auto"
-            />
+            /> */}
+            <p className="mx-auto h-10 w-auto text-center">🗓️ 🎞️</p>
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               {isSignup ? 'Sign up now' : 'Sign in to your account'}
             </h2>
